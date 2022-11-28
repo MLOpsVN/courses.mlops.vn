e78d1f8cca2744089129fe6d3902f657c336f07f
Tung Dao <tungdao17@gmail.com>
2022-11-28 11:32:12 +0800

    Add deployment files

---
+++ deployment/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cmd=$1
+
+if [[ -z "$cmd" ]]; then
+    echo "Missing command"
+    exit 1
+fi
+
+gen_image_info() {
+    IMAGE_PATH="${IMAGE_PATH:-mlopsvn/courses.mlops.vn}"
+    IMAGE_TAG="${IMAGE_TAG:-$(git describe --always)}"
+}
+
+build() {
+    gen_image_info
+    docker build --tag $IMAGE_PATH:$IMAGE_TAG -f deployment/Dockerfile .
+    docker tag $IMAGE_PATH:$IMAGE_TAG $IMAGE_PATH:latest
+}
+
+push() {
+    gen_image_info
+    docker push $IMAGE_PATH:$IMAGE_TAG
+    docker push $IMAGE_PATH:latest
+}
+
+shift
+
+case $cmd in
+build)
+    build "$@"
+    ;;
+push)
+    push "$@"
+    ;;
+*)
+    echo -n "Unknown command: $cmd"
+    exit 1
+    ;;
+esac
