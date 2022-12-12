0f32d3dc9ee7d87cb43aef35c93b292df082c3f9
Tung Dao <tungdao17@gmail.com>
2022-12-12 19:01:43 +0800

    Update script

--- scripts/course_export.sh
+++ scripts/course_export.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+rm site
 mkdocs build
-
 mv site/ mlopsvn-crash-course/
 
+rm mlopsvn-crash-course.zip
 zip -r mlopsvn-crash-course.zip mlopsvn-crash-course
-
 rm -rf mlopsvn-crash-course
